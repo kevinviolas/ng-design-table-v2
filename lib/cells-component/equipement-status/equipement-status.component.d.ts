2333d4bba6c14994adb5a7ca69b683120225246f
kevin violas <kevin.violas@nowbrains.com>
2022-05-12 15:55:07 +0200

    fix: ajout status

--- lib/cells-component/equipement-status/equipement-status.component.d.ts
+++ lib/cells-component/equipement-status/equipement-status.component.d.ts
@@ -1,13 +1,17 @@
 import { ChangeDetectorRef, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 import { TableService } from "../../table.service";
+import { TranslateService } from '../../translate.service';
 export declare class EquipementStatusComponent implements OnInit, OnChanges {
     private changeDetectorRefs;
     private service;
+    private translate;
     type: string;
     icon: string;
     size: number | string;
     css: any;
-    constructor(changeDetectorRefs: ChangeDetectorRef, service: TableService);
+    lang: string;
+    status: string;
+    constructor(changeDetectorRefs: ChangeDetectorRef, service: TableService, translate: TranslateService);
     ngOnInit(): void;
     ngOnChanges(changes: SimpleChanges): void;
 }
